9bf4266feddedb3c1ee660da3275a6d79992f50e
Pablo <pabloprietolopez94@outlook.es>
2025-08-27 19:44:00 +0200

    feat: add AppServidor.java

---
+++ src/app/chat/application/AppServidor.java
@@ -0,0 +1,13 @@
+package app.chat.application;
+
+import app.chat.javabeans.Marco;
+import app.chat.javabeans.PanelServidor;
+
+public class AppServidor {
+
+	public static void main (String[]args) {
+		
+		new Marco(new PanelServidor(), "App Servidor");
+		 //setBackground(Color.decode("#1D2026"));
+	}
+}
